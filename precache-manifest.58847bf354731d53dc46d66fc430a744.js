db672c6b33f3c23148aa496cddc6b1511903aec0
Axl Wang <axlwang1976@gmail.com>
2019-02-14 17:05:05 +0800

    boss01

---
+++ precache-manifest.58847bf354731d53dc46d66fc430a744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e37202fe17e4a549aad2",
+    "url": "/static/js/main.e37202fe.chunk.js"
+  },
+  {
+    "revision": "1b383c9b04a4f8484111",
+    "url": "/static/js/1.1b383c9b.chunk.js"
+  },
+  {
+    "revision": "e37202fe17e4a549aad2",
+    "url": "/static/css/main.54ce21e5.chunk.css"
+  },
+  {
+    "revision": "6441a08f2b2ef29960b459e5ea28a8ea",
+    "url": "/index.html"
+  }
+];
